8464ca8c3089956789993480b864cabe18b5bc0f
Ahaan Limaye <ahaan.limaye@gmail.com>
2021-05-13 13:11:46 -0400

    Added install

--- install.py
+++ install.py
@@ -10,6 +10,7 @@ def main():
     # Installs NLP Models
     stanza.install_corenlp(dir=corenlp_dir)
     stanza.download_corenlp_models(model='english-kbp', version='4.1.0', dir=corenlp_dir)
+    stanza.download('en')
 
 # Runs Main function
 if __name__ == '__main__':
